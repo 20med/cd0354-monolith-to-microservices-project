d1c6c7b6b64c1ec65a49db4d50f48bd2a826791c
“20med” <medgov20@gmail.com>
2023-03-21 21:15:22 +0200

    add profile to user api

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=admin1234
+export POSTGRES_HOST=cdr.c80abcbor1wj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=fwd-3rd-project-s3-bucket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export AWS_KEY=AKIAXZ32GG6MXBKNE3WS
+export AWS_SECRET=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
+export URL=http://localhost:8100
